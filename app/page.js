6a90d3d373d6113fc8ee4a68bea62a45258ebd99
varun <varun.govindarajan1@gmail.com>
2025-07-10 09:29:14 +0530

    Initial commit from Create Next App

---
+++ app/page.js
@@ -0,0 +1,10 @@
+'use client'
+import {useState} from "react";
+
+export default function Home(){
+  const[count,setCount]=useState(0)
+  return {
+    <div className="flex flex-col justify-center item-center gap-5 text">
+
+  }
+}
